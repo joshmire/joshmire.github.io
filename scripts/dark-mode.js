f301361d74d435c86b6d7a8192903e732c186c28
joshmire <the.doctor@joshmire.com>
2021-11-16 19:09:52 -0500

    Initial commit of new site 11-16-21

--- scripts/dark-mode.js
+++ scripts/dark-mode.js
@@ -5,12 +5,16 @@ function setTheme(){
     document.body.classList.add("dark-mode");
 	document.querySelector("#dark-mode-btn").classList.remove("fa-moon");
 	document.querySelector("#dark-mode-btn").classList.add("fa-sun");
+	document.getElementById("about-pic").setAttribute("href", "images/me/02.jpg");
+	document.getElementById("about-pic").setAttribute("x", "-30");
 	sessionStorage.setItem("dark", "true")
   }
   else{
     document.body.classList.remove("dark-mode");
 	document.querySelector("#dark-mode-btn").classList.remove("fa-sun");
 	document.querySelector("#dark-mode-btn").classList.add("fa-moon");
+	document.getElementById("about-pic").setAttribute("href", "images/me/01.jpg");
+	document.getElementById("about-pic").setAttribute("x", "0");
 	sessionStorage.setItem("dark", "false")
   }
 }
---
+++ scripts/footer.js
@@ -0,0 +1,3 @@
+jQuery(function(){
+	jQuery(".footer").html("<i><p>Created by Joshua Mire</p><p>Last Updated: Nov. 16, 2021</p></i>"); 
+});
