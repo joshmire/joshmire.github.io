c68843d57db5c74539bd39c9f7a89e7328552946
Joshua Mire <joshmire@gmail.com>
2021-11-22 14:14:58 -0500

    updated footer 11-22-21

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,3 +1,3 @@
 jQuery(function(){
-	jQuery(".footer").html("<i><p>Created by Joshua Mire</p><p>Last Updated: Nov. 16, 2021</p></i>"); 
+	jQuery(".footer").html("<i><p>Created by Joshua Mire</p><p>Last Updated: Nov. 22, 2021</p></i>"); 
 });
