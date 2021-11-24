3ec5b1facdad70f3593caae3c458fb46564a1ea9
Joshua Mire <joshmire@gmail.com>
2021-11-24 08:14:11 -0500

    updated title attributes of links

--- scripts/dark-mode.js
+++ scripts/dark-mode.js
@@ -5,16 +5,23 @@ function setTheme(){
     document.body.classList.add("dark-mode");
 	document.querySelector("#dark-mode-btn").classList.remove("fa-moon")
 	document.querySelector("#dark-mode-btn").classList.add("fa-sun")
-	document.getElementById("about-pic").setAttribute("href", "images/me/02.jpg")
-	document.getElementById("about-pic").setAttribute("x", "-30")
+	document.getElementById("dark-mode-link").setAttribute("title", "Light Mode")
+	if (document.getElementById("about-pic") != null) {
+		document.getElementById("about-pic").setAttribute("href", "images/me/02.jpg")
+		document.getElementById("about-pic").setAttribute("x", "-30")
+	}
+
 	sessionStorage.setItem("dark", "true")
   }
   else{
     document.body.classList.remove("dark-mode")
 	document.querySelector("#dark-mode-btn").classList.remove("fa-sun")
 	document.querySelector("#dark-mode-btn").classList.add("fa-moon")
-	document.getElementById("about-pic").setAttribute("href", "images/me/01.jpg")
-	document.getElementById("about-pic").setAttribute("x", "0")
+	document.getElementById("dark-mode-link").setAttribute("title", "Dark Mode")
+	if (document.getElementById("about-pic") != null) {
+		document.getElementById("about-pic").setAttribute("href", "images/me/01.jpg")
+		document.getElementById("about-pic").setAttribute("x", "0")
+	}
 	sessionStorage.setItem("dark", "false")
   }
 }
